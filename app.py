e5f4caf9227030dd4a1e7303d3943410cdf69ae7
Pranil Sakpal <pranilsakpal10@gmail.com>
2026-06-21 23:09:09 +0530

    flask learning done

--- app.py
+++ app.py
@@ -34,11 +34,21 @@ def show():
     print(allTodo)
     return 'Show Page'
 
-@app.route('/update')
-def update():
-    allTodo = Todo.query.all()
-    print(allTodo)
-    return 'update Page'
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    if request.method=='POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+    
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html', todo=todo)
+    
 
 @app.route('/delete/<int:sno>')
 def delete(sno):
